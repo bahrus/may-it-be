e83160a555b2796db8978bed9543340bd4323063
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-12-23 05:34:04 -0500

    0.0.2

--- types.d.ts
+++ types.d.ts
@@ -6,13 +6,15 @@ import { INotifyMap } from 'be-noticed/types';
 import { IObserveMap } from 'be-observant/types';
 import { BeSwitchedVirtualProps } from 'be-switched/types';
 import { BeTransformativeVirtualProps } from 'be-transformative/types';
+import { BeLoadedVirtualProps } from 'be-loaded/types';
 
-export interface MayItBe<Self, Props, Actions>{
-    beActive: BeActiveVirtualProps,
-    beFerried: BeFerriedVirtualProps,
-    beIntersectional: INotifyMap<Self, Props, Actions>,
-    beNoticed: INotifyMap<Self, Props, Actions>,
-    beObservant: IObserveMap<Self, Props, Actions>,
-    beSwitched: BeSwitchedVirtualProps,
-    beTransformative: BeTransformativeVirtualProps,
+export interface MayItBe<Self = any, Props = any, Actions = any>{
+    beActive?: BeActiveVirtualProps,
+    beFerried?: BeFerriedVirtualProps,
+    beIntersectional?: INotifyMap<Self, Props, Actions>,
+    beLoaded?: BeLoadedVirtualProps,
+    beNoticed?: INotifyMap<Self, Props, Actions>,
+    beObservant?: IObserveMap<Self, Props, Actions>,
+    beSwitched?: BeSwitchedVirtualProps,
+    beTransformative?: any,
 }
