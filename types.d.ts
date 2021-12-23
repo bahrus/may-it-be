9b091815625f299123ddc2d7030709eb4f270fd6
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-12-22 20:34:55 -0500

    init

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,12 @@
 import {BeActiveVirtualProps} from 'be-active/types';
 import {BeFerriedVirtualProps} from 'be-ferried/types';
 import { BeIntersectionalVirtualProps } from 'be-intersectional/types';
+import { BeMetamorphicVirtualProps } from 'be-metamorphic/types';
+import { INotifyMap } from 'be-noticed/types'; 
 
 export interface MayItBe<Self, Props, Actions>{
     beActive: BeActiveVirtualProps,
     beFerried: BeFerriedVirtualProps,
-    beIntersectional: BeIntersectionalVirtualProps,
+    beIntersectional: INotifyMap<Self, Props, Actions>,
+    beNoticed: INotifyMap<Self, Props, Actions>,
 }
