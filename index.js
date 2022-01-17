fc2d8e9432ad4c5f985c6d0fd12ae67f2fc16c76
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-01-17 10:19:35 -0500

    0.0.72

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ ${scriptRef}
 <script type=module>
     if(customElements.get('be-active') === undefined){
         import('be-active/be-active.js').catch(err => {
-            import('https://esm.run/be-active/be-active.js');
+            import('https://esm.run/be-active@0.0.15/be-active.js');
         });
     }
 </script>
