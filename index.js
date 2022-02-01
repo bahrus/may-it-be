3735605a77b9fa1380bf2f649670536d49621ea0
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-02-01 03:30:26 -0500

    0.0.121

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ document.body.insertAdjacentHTML('beforeend', \`${mainTemplate}\`);`;
     <!---->
     <template shadowroot="open">
         <template be-active>
-            <script data-version=0.0.15 id="be-importing/be-importing.js"></script>
+            <script data-version=0.0.16 id="be-importing/be-importing.js"></script>
             <script data-version=0.0.31 id="be-definitive/be-definitive.js" data-when=be-importing></script>${beExportable}
             ${dependencies.map(d => html `<script data-when=be-importing id="${d}"></script>`).join('\n')}
             ${globalStylePaths.map(p => html `<link rel="stylesheet" href="${p}">`).join('\n')}
