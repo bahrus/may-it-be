225e5a4ec01131abb32528fcbd0e5ca1201be2c5
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-02-01 03:05:47 -0500

    0.0.120

--- index.ts
+++ index.ts
@@ -51,7 +51,7 @@ encodeAndWrite(js);
     <!---->
     <template shadowroot="open">
         <template be-active>
-            <script data-version=0.0.14 id="be-importing/be-importing.js"></script>
+            <script data-version=0.0.15 id="be-importing/be-importing.js"></script>
             <script data-version=0.0.31 id="be-definitive/be-definitive.js" data-when=be-importing></script>${beExportable}
             ${dependencies.map(d => html`<script data-when=be-importing id="${d}"></script>`).join('\n')}
             ${globalStylePaths.map(p => html`<link rel="stylesheet" href="${p}">`).join('\n')}
