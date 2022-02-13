e9fc24ff7f31e7154beac15fe82d24e5c9913901
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-02-13 13:25:15 -0500

    0.0.125

--- doc.js
+++ doc.js
@@ -35,17 +35,22 @@ export class CustomElementManifestGenerator {
             const { properties } = definition;
             if (properties === undefined)
                 continue;
-            const { tagName } = properties;
+            const { tagName, src } = properties;
             if (tagName === undefined)
                 continue;
             const enm = tagName.enum;
             const name = this.getStringVal(enm);
+            let path = '';
+            if (src !== undefined) {
+                const srcEnm = src.enum;
+                path = this.getStringVal(srcEnm);
+            }
             if (name === '')
                 continue;
             const declarations = [];
             const module = {
                 kind: 'javascript-module',
-                path: 'tbd',
+                path,
                 declarations: declarations,
             };
             this.generateDeclarations(def, name, properties, declarations);
@@ -54,33 +59,68 @@ export class CustomElementManifestGenerator {
     }
     generateDeclarations(name, tagName, properties, declarations) {
         const { props, methods } = properties;
-        const { $ref } = props;
         const members = [];
-        if ($ref !== undefined) {
-            const split = $ref.split('/');
-            let ctx = this.#wcInfo;
-            let first = true;
-            for (const s of split) {
-                if (first) {
-                    first = false;
-                    continue;
+        if (props !== undefined) {
+            //props
+            const { $ref } = props;
+            if ($ref !== undefined) {
+                const split = $ref.split('/');
+                let ctx = this.#wcInfo;
+                let first = true;
+                for (const s of split) {
+                    if (first) {
+                        first = false;
+                        continue;
+                    }
+                    ctx = ctx[s];
                 }
-                ctx = ctx[s];
+                const propsDef = ctx;
+                if (propsDef === undefined)
+                    return;
+                const props = propsDef.properties;
+                if (props !== undefined) {
+                    for (const prop in props) {
+                        const { type, description } = props[prop];
+                        const member = {
+                            kind: 'field',
+                            name: prop,
+                            description,
+                        };
+                        members.push(member);
+                    }
+                }
             }
-            const propsDef = ctx;
-            if (propsDef === undefined)
-                return;
-            const props = propsDef.properties;
-            if (props === undefined)
-                return;
-            for (const prop in props) {
-                const { type, description } = props[prop];
-                const member = {
-                    kind: 'field',
-                    name: prop,
-                    description,
-                };
-                members.push(member);
+        }
+        if (methods !== undefined) {
+            //methods
+            const { $ref } = methods;
+            if ($ref !== undefined) {
+                const split = $ref.split('/');
+                let ctx = this.#wcInfo;
+                let first = true;
+                for (const s of split) {
+                    if (first) {
+                        first = false;
+                        continue;
+                    }
+                    ctx = ctx[s];
+                }
+                const methodsDef = ctx;
+                console.log(methodsDef);
+                if (methodsDef === undefined)
+                    return;
+                const methods = methodsDef.properties;
+                if (methods !== undefined) {
+                    for (const method in methods) {
+                        const { type, description } = methods[method];
+                        const member = {
+                            kind: 'method',
+                            name: method,
+                            description,
+                        };
+                        members.push(member);
+                    }
+                }
             }
         }
         const newDeclaration = {
--- doc.ts
+++ doc.ts
@@ -37,15 +37,20 @@ export class CustomElementManifestGenerator{
             const definition = definitions[def];
             const {properties} = definition;
             if(properties === undefined) continue;
-            const {tagName} = properties;
+            const {tagName, src} = properties;
             if(tagName === undefined) continue;
             const enm = tagName.enum;
             const name = this.getStringVal(enm);
+            let path = '';
+            if(src !== undefined){
+                const srcEnm = src.enum;
+                path = this.getStringVal(srcEnm);
+            }
             if(name === '') continue;
             const declarations: CustomElement[] = [];
             const module: JavaScriptModule = {
                 kind: 'javascript-module',
-                path: 'tbd',
+                path,
                 declarations: declarations as Declaration[],
             };
             this.generateDeclarations(def, name, properties, declarations);
@@ -55,33 +60,70 @@ export class CustomElementManifestGenerator{
 
     generateDeclarations(name: string, tagName: string, properties: {[key: string]: SchemaProperty}, declarations: CustomElement[]){
         const {props, methods} = properties;
-        const {$ref} = props;
         const members: ClassMember[] = [];
-        if($ref !== undefined){
-            const split = $ref.split('/');
-            let ctx = this.#wcInfo as any;
-            let first = true;
-            for(const s of split){
-                if(first){
-                    first = false;
-                    continue;
+        if(props !== undefined){
+            //props
+            const {$ref} = props;
+        
+            if($ref !== undefined){
+                const split = $ref.split('/');
+                let ctx = this.#wcInfo as any;
+                let first = true;
+                for(const s of split){
+                    if(first){
+                        first = false;
+                        continue;
+                    }
+                    ctx = ctx[s];
                 }
-                ctx = ctx[s];
-            }
-            const propsDef = ctx as SchemaDefinition;
-            if(propsDef === undefined) return;
-            const props = propsDef.properties;
-            if(props === undefined) return;
-            for(const prop in props){
-                const {type, description} = props[prop];
-                const member: PropertyLike & ClassMember = {
-                    kind: 'field',
-                    name: prop,
-                    description,
-                };
-                members.push(member);
+                const propsDef = ctx as SchemaDefinition;
+                if(propsDef === undefined) return;
+                const props = propsDef.properties;
+                if(props !== undefined) {
+                    for(const prop in props){
+                        const {type, description} = props[prop];
+                        const member: PropertyLike & ClassMember = {
+                            kind: 'field',
+                            name: prop,
+                            description,
+                        };
+                        members.push(member);
+                    }
+                }
             }
         }
+        if(methods !== undefined){
+            //methods
+            const {$ref} = methods;
+            if($ref !== undefined){
+                const split = $ref.split('/');
+                let ctx = this.#wcInfo as any;
+                let first = true;
+                for(const s of split){
+                    if(first){
+                        first = false;
+                        continue;
+                    }
+                    ctx = ctx[s];
+                }
+                const methodsDef = ctx as SchemaDefinition;
+                console.log(methodsDef);
+                if(methodsDef === undefined) return;
+                const methods = methodsDef.properties;
+                if(methods !== undefined) {
+                    for(const method in methods){
+                        const {type, description} = methods[method];
+                        const member: PropertyLike & ClassMember = {
+                            kind: 'method',
+                            name: method,
+                            description,
+                        };
+                        members.push(member);
+                    }
+                }
+            }
+        }
+        
         const newDeclaration: CustomElement = {
             tagName,
             name,
