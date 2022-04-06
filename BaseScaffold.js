cfeb58528aa35249e2a6366f30d66ef09a6adbbb
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-04-06 13:08:19 -0400

    0.0.145

--- BaseScaffold.js
+++ BaseScaffold.js
@@ -71,9 +71,9 @@ ${Object.keys(categories).map(category => {
 ${Array.from(unclassifiedMembers).map(memberKey => this.renderMember(memberKey))}
 </form>
 <template be-active>
-    <script data-version=0.0.51 id=be-loaded/be-loaded.js data-when=be-importing ></script>
-    <script data-version=0.0.101 id=be-observant/be-observant.js data-when=be-loaded ></script>
-    <script data-version=0.0.57 id=be-noticed/be-noticed.js data-when=be-observant ></script>
+    <script data-version=0.0.52 id=be-loaded/be-loaded.js data-when=be-importing ></script>
+    <script data-version=0.0.108 id=be-observant/be-observant.js data-when=be-loaded ></script>
+    <script data-version=0.0.58 id=be-noticed/be-noticed.js data-when=be-observant ></script>
 </template>
 <be-hive></be-hive>
 `;
